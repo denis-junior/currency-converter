c856e52c6c46fa89bc75d35fd36ebb282d8f481b
Denis <denis_jr2001@hotmail.com>
2022-07-15 21:37:03 -0300

    just formating

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,30 +1,28 @@
-import React, { useEffect } from 'react';
-import { BrowserRouter, Route, Routes } from 'react-router-dom';
-import { PrivateHomepage } from './pages/PrivateHomepage';
-import { Login } from './pages/Login';
-import { Register } from './pages/Register';
-import { PublicHomepage } from './pages/PublicHomepage';
-import { PrivateHomepageContextProvider } from './contexts/PrivateHomepageContext';
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+import { PrivateHomepage } from "./pages/PrivateHomepage";
+import { Login } from "./pages/Login";
+import { Register } from "./pages/Register";
+import { PublicHomepage } from "./pages/PublicHomepage";
+import { PrivateHomepageContextProvider } from "./contexts/PrivateHomepageContext";
 function App() {
-
-  //state for direction of the registers flow or use BrowserRouter
-
   return (
-
     <BrowserRouter>
       <Routes>
         <Route path="/" element={<PublicHomepage />} />
-        <Route path="/app" element={<>
-          <PrivateHomepageContextProvider>
-            <PrivateHomepage/>
-          </PrivateHomepageContextProvider>
-        </>
-          } />
+        <Route
+          path="/app"
+          element={
+            <>
+              <PrivateHomepageContextProvider>
+                <PrivateHomepage />
+              </PrivateHomepageContextProvider>
+            </>
+          }
+        />
         <Route path="/session" element={<Login />} />
         <Route path="/register" element={<Register />} />
       </Routes>
     </BrowserRouter>
-
   );
 }
 
